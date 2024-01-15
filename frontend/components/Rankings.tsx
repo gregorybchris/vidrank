a0da8ff21531045826aeb9617019e2d0c2394b49
Chris Gregory <gregorybchris@gmail.com>
2024-01-15 13:33:02 -0800

    Simplify paging UI

--- frontend/components/Rankings.tsx
+++ frontend/components/Rankings.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { ClockCountdown, WarningOctagon } from "@phosphor-icons/react";
+import {
+  CaretLeft,
+  CaretRight,
+  ClockCountdown,
+  WarningOctagon,
+} from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 
 import { Client } from "@/lib/client";
@@ -10,6 +15,11 @@ import { Button } from "widgets/Button";
 import { Video } from "./Video";
 
 const PAGE_SIZE = 8;
+
+type PageVisibility = {
+  visible: boolean;
+  opacity: number;
+};
 
 export function Rankings() {
   const client = new Client();
@@ -39,11 +49,30 @@ export function Rankings() {
       });
   }
 
-  function getPageRankings(pageNumber: number): Ranking[] {
-    return rankings.slice(
-      pageNumber * PAGE_SIZE,
-      pageNumber * PAGE_SIZE + PAGE_SIZE,
-    );
+  function getPageRankings(page: number): Ranking[] {
+    return rankings.slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE);
+  }
+
+  function getPageVisibility(page: number): PageVisibility {
+    const dist = Math.abs(page - currentPage);
+    const maxDist = 6.0;
+    return {
+      visible: dist < maxDist,
+      opacity: (maxDist - dist) / maxDist,
+    };
+  }
+
+  function canPageToOffset(offset: number) {
+    const newPage = currentPage + offset;
+    if (newPage < 0 || newPage >= numPages) {
+      return false;
+    }
+    return true;
+  }
+
+  function offsetPage(offset: number) {
+    if (!canPageToOffset(offset)) return;
+    setCurrentPage((currentPage) => currentPage + offset);
   }
 
   return (
@@ -84,21 +113,55 @@ export function Rankings() {
               </div>
             ))}
           </div>
-          <div className="flex w-full flex-row justify-center space-x-[2px]">
-            {new Array(numPages)
-              .fill(0)
-              .map((_, x) => x + 1)
-              .map((page) => (
-                <Button
-                  className={cn(
-                    "px-3",
-                    page === currentPage + 1 && "bg-stone-300 text-stone-800",
-                  )}
-                  key={page}
-                  text={`${page}`}
-                  onClick={() => setCurrentPage(page - 1)}
-                />
-              ))}
+          <div className="flex w-full flex-col items-center justify-center space-y-1">
+            <div className="flex flex-row justify-center space-x-[2px]">
+              {new Array(numPages)
+                .fill(0)
+                .map((_, x) => x)
+                .map((page) => {
+                  const pageVisibility = getPageVisibility(page);
+                  return (
+                    <Button
+                      className={cn(
+                        "px-3",
+                        page === currentPage && "bg-stone-300 text-stone-800",
+                        !pageVisibility.visible && "hidden",
+                        pageVisibility.opacity < 1.0 && "opacity-90",
+                        pageVisibility.opacity < 0.8 && "opacity-70",
+                        pageVisibility.opacity < 0.6 && "opacity-50",
+                        pageVisibility.opacity < 0.4 && "opacity-30",
+                      )}
+                      key={page}
+                      text={`${page + 1}`}
+                      onClick={() => setCurrentPage(page)}
+                    />
+                  );
+                })}
+            </div>
+            <div className="flex flex-row space-x-1">
+              <div
+                className={cn(
+                  "p-1",
+                  canPageToOffset(-1)
+                    ? "cursor-pointer rounded transition-all hover:bg-stone-300"
+                    : "opacity-40",
+                )}
+                onClick={() => offsetPage(-1)}
+              >
+                <CaretLeft size={24} color="#3e8fda" weight="duotone" />
+              </div>
+              <div
+                className={cn(
+                  "p-1",
+                  canPageToOffset(1)
+                    ? "cursor-pointer rounded transition-all hover:bg-stone-300"
+                    : "opacity-40",
+                )}
+                onClick={() => offsetPage(1)}
+              >
+                <CaretRight size={24} color="#3e8fda" weight="duotone" />
+              </div>
+            </div>
           </div>
         </div>
       )}
