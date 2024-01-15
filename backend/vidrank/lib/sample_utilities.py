fb73a9d7c05db92593b5bf78a47d1ddc2d6c4f96
Chris Gregory <gregorybchris@gmail.com>
2024-01-14 19:39:09 -0800

    Ensure 6 videos are found

--- backend/vidrank/lib/sample_utilities.py
+++ backend/vidrank/lib/sample_utilities.py
@@ -6,6 +6,8 @@ from vidrank.lib.youtube.video import Video
 
 def sample_videos(app_state: AppState, n_videos: int) -> List[Video]:
     playlist = app_state.youtube_facade.get_playlist(app_state.playlist_id)
-    video_ids = app_state.rng.choice(playlist.video_ids, n_videos, replace=False)
-    videos = list(app_state.youtube_facade.iter_videos(video_ids))
+    videos: List[Video] = []
+    while len(videos) < n_videos:
+        video_ids = app_state.rng.choice(playlist.video_ids, n_videos - len(videos), replace=False)
+        videos.extend(app_state.youtube_facade.iter_videos(video_ids))
     return videos
