f590964b48ee2b4f245c455d3938a411e85c8493
Chris Gregory <gregorybchris@gmail.com>
2024-01-15 00:02:05 -0800

    Paging on rankings page

--- frontend/components/Rankings.tsx
+++ frontend/components/Rankings.tsx
@@ -5,12 +5,19 @@ import { useEffect, useState } from "react";
 
 import { Client } from "@/lib/client";
 import { Ranking } from "@/lib/ranking";
+import { cn } from "@/lib/styleUtilities";
+import { Button } from "widgets/Button";
 import { Video } from "./Video";
+
+const PAGE_SIZE = 8;
 
 export function Rankings() {
   const client = new Client();
   const [rankings, setRankings] = useState<Ranking[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const numPages = Math.ceil(rankings.length / PAGE_SIZE);
 
   useEffect(() => {
     fetchRankings();
@@ -32,6 +39,13 @@ export function Rankings() {
       });
   }
 
+  function getPageRankings(pageNumber: number): Ranking[] {
+    return rankings.slice(
+      pageNumber * PAGE_SIZE,
+      pageNumber * PAGE_SIZE + PAGE_SIZE,
+    );
+  }
+
   return (
     <>
       {loading && (
@@ -49,25 +63,43 @@ export function Rankings() {
       )}
 
       {!loading && rankings.length > 0 && (
-        <div className="flex flex-wrap justify-center py-10">
-          {rankings.map((ranking, i) => (
-            <div key={i} className="relative">
-              <a
-                href={`https://www.youtube.com/watch?v=${ranking.video.id}`}
-                target="_blank"
-              >
-                <Video
-                  video={ranking.video}
-                  onClick={() => {}}
-                  action="nothing"
-                  isCurrent={false}
+        <div className="flex h-full flex-col justify-center space-y-10">
+          <div className="flex flex-wrap justify-center">
+            {getPageRankings(currentPage).map((ranking, i) => (
+              <div key={i} className="relative">
+                <a
+                  href={`https://www.youtube.com/watch?v=${ranking.video.id}`}
+                  target="_blank"
+                >
+                  <Video
+                    video={ranking.video}
+                    onClick={() => {}}
+                    action="nothing"
+                    isCurrent={false}
+                  />
+                  <div className="absolute right-3 top-2 flex h-8 w-8 flex-col items-center justify-center rounded-full bg-stone-900/70 text-center text-sm text-stone-100">
+                    {Math.floor(ranking.score)}
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+          <div className="flex w-full flex-row justify-center space-x-[2px]">
+            {new Array(numPages)
+              .fill(0)
+              .map((_, x) => x + 1)
+              .map((page) => (
+                <Button
+                  className={cn(
+                    "px-3",
+                    page === currentPage + 1 && "bg-stone-300 text-stone-800",
+                  )}
+                  key={page}
+                  text={`${page}`}
+                  onClick={() => setCurrentPage(page - 1)}
                 />
-                <div className="absolute right-0 top-0 h-10 w-10 rounded-full border-2 border-stone-200 bg-stone-900 pt-2 text-center text-stone-100">
-                  {Math.floor(ranking.score)}
-                </div>
-              </a>
-            </div>
-          ))}
+              ))}
+          </div>
         </div>
       )}
     </>
