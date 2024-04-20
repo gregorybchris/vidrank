5e6167d78684c7bf5571d0317b970412fea704c4
Chris Gregory <gregorybchris@gmail.com>
2024-04-20 02:46:34 -0700

    Add CLI command for displaying a record

--- backend/vidrank/cli/cli.py
+++ backend/vidrank/cli/cli.py
@@ -4,6 +4,7 @@ from typing import Any
 import click
 
 from vidrank.app.app import App
+from vidrank.app.app_state import AppState
 
 logger = logging.getLogger(__name__)
 
@@ -23,3 +24,20 @@ def serve(debug: bool = False, **kwds: Any) -> None:
 
     with App.context(**kwds) as app:
         app.start()
+
+
+@main.command()
+@click.argument("record_id", type=str)
+def record(record_id: str) -> None:
+    App.load_app_state()
+    app_state = AppState.get()
+    records = app_state.record_tracker.load()
+    for r in records:
+        if r.id == record_id:
+            for choice in r.choice_set.choices:
+                video_id = choice.video_id
+                for video in app_state.youtube_facade.iter_videos([video_id]):
+                    print(f"{video.id}: ({choice.action}) {video.title}")
+            break
+    else:
+        logger.error(f"Record with ID {record_id} not found")
