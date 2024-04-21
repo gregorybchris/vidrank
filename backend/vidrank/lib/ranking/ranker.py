ea09347301dfccd4b900bdbc0229a93eb630b654
Chris Gregory <gregorybchris@gmail.com>
2024-04-20 21:57:04 -0700

    Refactor ranker

--- backend/vidrank/lib/ranking/ranker.py
+++ backend/vidrank/lib/ranking/ranker.py
@@ -3,6 +3,7 @@ from typing import Dict, Iterator, List
 
 from trueskill import Rating, rate
 
+from vidrank.lib.action import Action
 from vidrank.lib.ranking.ranking import Ranking
 from vidrank.lib.record import Record
 
@@ -18,22 +19,25 @@ class Ranker:
     def iter_rankings(cls, records: List[Record]) -> Iterator[Ranking]:
         rating_map: Dict[str, Rating] = {}
         for comp in cls._get_comps(records):
-            if comp.winner_id not in rating_map:
-                rating_map[comp.winner_id] = Rating()
-            if comp.loser_id not in rating_map:
-                rating_map[comp.loser_id] = Rating()
-
-            winner_rating = rating_map[comp.winner_id]
-            loser_rating = rating_map[comp.loser_id]
-
-            rating_groups = [{comp.winner_id: winner_rating}, {comp.loser_id: loser_rating}]
-            comp_ratings = rate(rating_groups, ranks=[0, 1])
-            rating_map.update(comp_ratings[0])
-            rating_map.update(comp_ratings[1])
+            cls._update_ratings(rating_map, comp)
 
         sorted_ratings = sorted(rating_map.items(), key=lambda x: x[1].mu, reverse=True)
         for i, (video_id, rating) in enumerate(sorted_ratings):
             yield Ranking(video_id=video_id, rank=i + 1, rating=rating.mu)
+
+    @classmethod
+    def _update_ratings(cls, rating_map: Dict[str, Rating], comp: Comp) -> None:
+        for video_id in [comp.winner_id, comp.loser_id]:
+            if video_id not in rating_map:
+                rating_map[video_id] = Rating()
+
+        winner_rating = rating_map[comp.winner_id]
+        loser_rating = rating_map[comp.loser_id]
+
+        rating_groups = [{comp.winner_id: winner_rating}, {comp.loser_id: loser_rating}]
+        comp_ratings = rate(rating_groups, ranks=[0, 1])
+        rating_map.update(comp_ratings[0])
+        rating_map.update(comp_ratings[1])
 
     @classmethod
     def _get_comps(cls, records: List[Record]) -> Iterator[Comp]:
@@ -43,7 +47,7 @@ class Ranker:
                 for choice_b in record.choice_set.choices:
                     action_b = choice_b.action
 
-                    if (action_a, action_b) == ("select", "nothing"):
+                    if (action_a, action_b) == (Action.SELECT, Action.NOTHING):
                         yield Comp(choice_a.video_id, choice_b.video_id)
-                    if (action_a, action_b) == ("nothing", "select"):
+                    if (action_a, action_b) == (Action.NOTHING, Action.SELECT):
                         yield Comp(choice_b.video_id, choice_a.video_id)
