afa19dbdb61db9f32a196dcac3670d89a6d739c6
Chris Gregory <gregorybchris@gmail.com>
2024-06-30 12:19:18 -0700

    Add pytest

---
+++ backend/tests/test_version.py
@@ -0,0 +1,9 @@
+import semver
+from vidrank import __version__
+
+
+class TestVersion:
+    def test_version(self) -> None:
+        version = semver.VersionInfo.parse(__version__)
+        assert version.major == 0
+        assert version.minor > 0
